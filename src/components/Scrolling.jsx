7483826a893a135155dfe7f57e422a6331577b5b
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-08 21:54:32 -0400

    adding more styling

--- src/components/Scrolling.jsx
+++ src/components/Scrolling.jsx
@@ -7,9 +7,10 @@ const Scrolling = ({ myText }) => {
     const [key, setKey] = useState(1)
 
     const scroll = useSpring({
-        from: { transform: "translate(0, 90%)" },
-        to: { transform: 'translate(0, -90%)' },
-        config: { duration: 5000 },
+        from: { transform: "translate(-10px, 400px)", opacity: '100%', color: '#000', 'letter-spacing': '3px' },
+        to: { transform: 'translate(0, -20px)', opacity: '50%', color: '#ccc', 'letter-spacing': '-1px'},
+        // transform: 'translate3d(0px,0,0) scale(1) rotateX(0deg)'
+        config: { duration: 9000 },
         reset: true,
         onRest: () => {
             setKey(key + 1)
